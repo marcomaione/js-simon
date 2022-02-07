144cc5a355333e7e87191ca6969e7004798e6f9e
marco maione <maionemarco14@gmail.com>
2022-02-07 16:35:34 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 //genero 5 numeri casuali tra uno e 100
 
- n = 5;
+n = 5;
 
-if (n > 0 && n <= 100) {
-    testo = "";
-      for (i = 0; i < n; i++) {
-           numeri = Math.round(Math.random()*100 + 1);
-        if (i > 0) {
-             testo += ", ";
-        }
-         testo += numeri;
+testo = "";
+
+for (i = 0; i < n; i++) {
+
+    numeri = Math.round(Math.random() * 100 + 1);
+
+    if (i > 0) {
+
+        testo += ", ";
     }
+        testo += numeri;
+}
 
-    document.getElementById("random").innerHTML = testo;
+document.getElementById("random").innerHTML = testo;
      
-} 
      
   
   
